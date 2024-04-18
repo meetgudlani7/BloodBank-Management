3053874f87e9c86ef45c85889a2fb9de140a34ed
meetgudlani7 <meetgudlani7@gmail.com>
2024-04-18 18:15:32 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import flash,Flask,session,redirect,render_template,url_for,request
+from check import *
+
+app = Flask(__name__)
+app.secret_key="Blood"
+
+@app.route('/')
+def index():
+    return render_template("login.html")
+
+@app.route('/mainpage')
+def mainpage():
+    if "user" in session:
+        return render_template("index.html")
+    else:
+        return redirect(url_for("index"))
+
+
+@app.route('/login',methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        username = request.form["Username"]
+        Password = request.form["Password"]
+        if Login_check(username,Password):
+            session["user"] = username
+            return redirect(url_for("mainpage"))
+        else:
+            return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))
+    
+@app.route('/signup',methods=["POST","GET"])
+def signup():
+    if request.method == "POST":
+        username = request.form["Username"]
+        email = request.form["Email"]
+        Password = request.form["Password"]
+        if signup_check(username,email,Password):
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("index"))
+    else:
+        return redirect(url_for("index"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ check.py
@@ -0,0 +1,28 @@
+import mysql.connector as connector
+
+con = connector.connect(host='localhost',
+                        port='3306',
+                        user='root',
+                        password='Meet@2345',
+                        database='PROJECT')
+cursor = con.cursor()
+
+def Login_check(username,password):
+    Qstr = "SELECT name,pass from userdata where name=\"" + str(username) + "\"" "and pass=\"" + str(password) + "\""
+    cursor.execute(Qstr)
+    rows = cursor.fetchall()
+    if len(rows)==0:
+        return False
+    else:
+        return True
+    
+def signup_check(username,email,Password):
+    Qstr = "SELECT * from userdata where email=\"" + str(email) + "\""
+    cursor.execute(Qstr)
+    rows = cursor.fetchall()
+    if len(rows)==0:
+        Qstr = "insert into userdata values(\"{}\",\"{}\",\"{}\")".format(email,username,Password)
+        cursor.execute(Qstr)
+        con.commit()
+        return True
+    return False
