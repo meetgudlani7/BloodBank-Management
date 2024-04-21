33f9d9a4aa75207c0562652c85969f9ab9824e3a
meetgudlani7 <meetgudlani7@gmail.com>
2024-04-21 18:19:31 +0530

    css updated

--- check.py
+++ check.py
@@ -3,7 +3,7 @@ import mysql.connector as connector
 con = connector.connect(host='localhost',
                         port='3306',
                         user='root',
-                        password='HARSHsql@1234',
+                        password='Meet@2345',
                         database='PROJECT')
 cursor = con.cursor()
 
